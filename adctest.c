a587d5878cdc0156c5963eebf31846c2b9c37316
Jmedici09 <Jmedici09@gmail.com>
2018-02-07 15:19:04 -0500

    Update adctest.c

--- adctest.c
+++ adctest.c
@@ -15,6 +15,7 @@ DDRC = 0x00;
 PORTC = 0xFF;
 PORTB = 0x00;
 
+uint_16 ADCValue = (ADCH << 8) | (ADCL << 1);
 
 //VCC voltage reference
 //Use pin C5 as ADC input
@@ -24,8 +25,11 @@ ADMUX |= (1 << REFS0) | (1 << ADLAR) | (1 << MUX2) | (1 << MUX0);
 //Start ADC Conversion
 //Enable auto-trigger
 //Set prescaler to 8 so the ADC runs at 125 kHZ
-ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE) | (1 << ADPS1) | (1 << ADPS0);
+ADCSRA |= (1 << ADATE);
+ADCSRA |= (1 << ADPS1) | (1 << ADPS0);
+ADCSRA |= (1 << ADEN) | (1 << ADSC);
 
+	
 // Use the ADC in freerunning mode
 //ADCSRB |= ;
 
@@ -33,7 +37,7 @@ ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE) | (1 << ADPS1) | (1 << ADPS0)
 
     PORTB = ADCH;
 
-	/*	if(ADCH > 0xC0){
+	/*	if(ADCValue > 0xC000){
 			PORTB = 0xFF; 
 		} else{
 			PORTB = 0x00;
