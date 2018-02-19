5d842279da6cec45b9b08235fecd162c9164010b
Drew Cruey <andrew.cruey@gmail.com>
2018-02-19 13:13:11 -0500

    Update UART.c

--- Labs/Lab5/UART.c
+++ Labs/Lab5/UART.c
@@ -8,18 +8,32 @@
 #define BaudL ((F_CPU / (16 * Baud)) -1)
 
 void initUART();
+void initPWM();
 
 
 int main(){
-	
+	initUART();
+	initPWM();
+	while(1){
+		if(UCSR0A & ( 1<< RXC0)){//if receive complete flag
+			OCR0A = UDR0;//sets pwm equal to uart reg
+		}	
+	}
 	return 0;
 }
 
 void initUART(){
-	UCSR0A = ;
-	UCSR0B = ;
-	UCSR0C = (1 << UPM01) | (1 << UPM00) | (1 << UCSZ01) | (1 << UCSZ00);
+	UCSR0A = 0x00;//error, speed, multi processors, and flags we don't need for this
+	UCSR0B = (1 << RXEN0);//receive complete bit
+	UCSR0C = (1 << UPM01) | (1 << UPM00) | (1 << UCSZ01) | (1 << UCSZ00);//upm enables odd parity, sets character size to 8 bits
 	
 	UBRR0H = BaudH;
 	UBRR0L = BaudL;
+}
+
+void initPWM(){
+    
+    DDRD = 0xFF;
+    TCCR0A |= 0xFF; //set to fast PWM (timer register)
+
 }
