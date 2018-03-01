83bad5640144f98296218ec81d6bf26165790858
Jmedici09 <Jmedici09@gmail.com>
2018-03-01 16:40:39 -0500

    Updated Modules
    got rid of global variable and division

--- Labs/Modules/ExtInterrupts/ExtInterrupts.c
+++ Labs/Modules/ExtInterrupts/ExtInterrupts.c
@@ -2,8 +2,9 @@
 
 void initExtInterrupt0(){ // Rising edge trigger on int0 (PD2)
 	// int0 uses pin PD2
-	global uint8_t falling0 = 0;
-	
+	PIND &= ~(1 << PD2)
+	PORTD |= (1 << PD2)
+
 	EICRA |= (0 << ISC01) | (1 << ISC00);
 	EIMSK |= (1 << INT0);	
 	
@@ -19,17 +20,15 @@ void checkDistance0(){
 
 	TCNT1 = 0x00;
 	TCCR1B = 0x00; // turn counter 0 off
-	falling0 = 0;
 }
 
 ISR(INT0_vect){ // Toggle port C0 on rising edge of int0(PD2)
 	cli();
 	
-	if(falling0 == 0){
-		TCCR1B |= (1 << CS10); // turn counter 0 off
-		falling0 = 1;
+	if(((PIND & (1<<PD2)) == 1) && (TCCR1B & (1 << CS10) == 0)){
+		TCCR1B |= (1 << CS10); // turn counter 0 on
 	} else {
-		TCCR1B &= ~(1 << CS10);
+		TCCR1B &= ~(1 << CS10); // turn counter 0 off
 	}
 
 	sei();
--- Labs/Modules/PWM/PWM.c
+++ Labs/Modules/PWM/PWM.c
@@ -1,16 +1,14 @@
 #include "PWM.h"
 
-#define FULL 255	
-
 void initPWM(){
 	TCCR0A |= 0xFF; //set to fast PWM (timer register)
     TCCR0B |= (1 << CS00);
 }
 
-void setPWMA(uint8_t pmw_divide){
-	OCCRA = FULL / pwm_divide;
+void setPWMA(uint8_t duty_cycle){ // Number from 1- 255
+	OCCRA = duty_cycle;
 }
 
-void setPWMA(uint8_t pmw_divide){
-	OCCRB = FULL / pwm_divide;
+void setPWMA(uint8_t duty_cycle){ // Number from 1- 255
+	OCCRB = duty_cycle;
 }
